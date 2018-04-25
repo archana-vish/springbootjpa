646f94796f545f02dbb40dba7522d1f6d2f393b8
Archana Visveswaran <A.Visveswaran@greenwich.ac.uk>
2018-04-25 14:26:28 +0100

    Access from H2

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,10 +2,16 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Application {
@@ -15,6 +21,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Bean
     public CommandLineRunner demo(PersonRepository repository) {
@@ -52,6 +61,41 @@ public class Application {
                         log.info(targareyns.toString());
                     });
             log.info("");
+
+            log.info("Now access data from H2");
+            log.info("-----------------------");
+
+
+            log.info("");
+            log.info("Create a table");
+
+            jdbcTemplate.execute("DROP TABLE CUSTOMER IF EXISTS ");
+            jdbcTemplate.execute("CREATE TABLE CUSTOMER(" +
+                    "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+            log.info("");
+
+            // Batch update works only on objects
+            List<Object[]> names = Arrays.asList("Jon Snow","Daenerys Targaryen","Cersei Lanister", "Arya Stark", "Night King")
+                    .stream()
+                    .map(name -> name.split("\\s"))
+                    .collect(Collectors.toList());
+
+            log.info("Insert into table");
+            log.info("-----------------");
+
+            jdbcTemplate.batchUpdate("INSERT INTO CUSTOMER(first_name, last_name) VALUES(?,?)", names);
+
+            log.info("Get all records");
+            log.info("---------------");
+
+            jdbcTemplate.query(
+                    "SELECT id, first_name, last_name FROM customer ", new Object[] { },
+                    (rs, rowNum) -> new Customer( rs.getString("first_name"), rs.getString("last_name"))
+            ).forEach(customer -> log.info(customer.toString()));
+
+
+
 
         };
     }
